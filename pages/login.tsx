4754a78c2371dcff3e0de576b8784e2bae73636e
Genaro Garcia <genaro.dev@outlook.com>
2024-02-09 14:06:10 -0600

    Capitalized React component name for adherence to naming conventions

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { ChangeEvent, MouseEvent, useEffect, useState } from 'react';
 
-const login = () => {
+const Login = () => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const [isLoading, setIsLoading] = useState(true);
@@ -119,4 +119,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
